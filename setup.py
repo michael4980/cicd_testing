e89845b5901a9c161fc338ee342729ad85cd0af6
michael4980 <migel1999@bk.ru>
2023-03-30 16:03:54 +0300

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pushingtest_4980",
-    version="0.1.1",
+    version="0.1.2",
     author="Michael Chupilnick",
     author_email="your_email@example.com",
     description="CI/CD test package",
